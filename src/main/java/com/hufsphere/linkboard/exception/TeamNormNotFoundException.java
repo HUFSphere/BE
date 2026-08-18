f20e72303f51808d327cb1352388d3243eb8a029
박재영 <pjyng115@naver.com>
2026-08-18 16:28:42 +0900

    fix: 팀 관행 수정/삭제 시 500 대신 404 반환하도록 수정
    TeamNormService가 없는 normId에 대해 NoSuchElementException을 던졌으나
    전용 핸들러가 없어 GlobalExceptionHandler의 Exception.class 폴백으로
    빠져 500이 반환되던 문제. TeamNormNotFoundException을 추가하고
    404로 매핑

---
+++ src/main/java/com/hufsphere/linkboard/exception/TeamNormNotFoundException.java
@@ -0,0 +1,8 @@
+package com.hufsphere.linkboard.exception;
+
+public class TeamNormNotFoundException extends RuntimeException {
+
+    public TeamNormNotFoundException(String message) {
+        super(message);
+    }
+}
